154ff91e43dbc3b1bf442a76828472f4994eff8f
Yoann <yoannpascalbouix@gmail.com>
2021-09-18 18:56:49 +0200

    Init typescript react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='todo'>
+        <h1>Shop</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
